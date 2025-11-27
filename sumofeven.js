579f02a927481321d26671bd6bdd39fbd520e84d
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-11-27 15:26:10 -0500

    code completed for sum of even numbers script

--- sumofeven.js
+++ sumofeven.js
@@ -1,7 +1,25 @@
 arrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function sumOfTripledEvens(array) {
+let even;
+function filterEvenNumbers(array) {
   even = array.filter((x) => x % 2 === 0);
   console.log(even);
 }
+let multipleofevens;
+function multipleOfEvens(evenNumbers) {
+  return (multipleofevens = evenNumbers.map((num) => num * 3));
+}
 
-sumOfTripledEvens(arrays);
+filterEvenNumbers(arrays);
+multipleOfEvens(even);
+console.log(even);
+
+let sum = 0;
+function sumOfEvens(multipleofevens) {
+  for (let i = 0; i < multipleofevens.length; i++) {
+    sum = sum + multipleofevens[i];
+  }
+}
+
+console.log("multiple of evens", multipleofevens);
+sumOfEvens(multipleofevens);
+console.log("sum of evens", sum);
