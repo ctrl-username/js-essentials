7442044f3d8dc5ee6557d7c9eed5ba1ba6268d34
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-11-28 06:16:39 -0500

    refactored code to look more succint (sumofeven.js)

--- sumofeven.js
+++ sumofeven.js
@@ -1,15 +1,10 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function sumOfTripleEvens(numbers) {
-  //filter and store even numbers to the evens variable
-  const evens = numbers.filter((x) => x % 2 === 0);
-  //multiplies each even number by 3 and stores them in a new array called triple of evens
-  const tripleOfEvens = evens.map((x) => x * 3);
-  //adds up all the multiplied even numbers and stores em in sum of evens
-  const sumOfEvens = tripleOfEvens.reduce(
-    (number, anothernumber) => number + anothernumber
-  );
-  return sumOfEvens;
+  return (evens = numbers
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 3)
+    .reduce((sum, num) => sum + num));
 }
 
 console.log(sumOfTripleEvens(array));
