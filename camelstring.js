4c36c7049aa27615c06b5c86a506a1908a08f87a
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-01 21:35:52 +0100

    making attempt for creating camelizes function

---
+++ camelstring.js
@@ -0,0 +1,9 @@
+//create function
+// function takes string seperated by dash as arguements
+// functions uses dash as delimeter for string
+// functions brakes string into array from delimeter
+// function converts string array to string with camel case
+
+function camelizes(str) {
+  str.split("-");
+}
