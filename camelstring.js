237bf7eb8e489d22399af53ff076efecbf913f2d
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-01 22:06:21 +0100

    did what i could :)

--- camelstring.js
+++ camelstring.js
@@ -4,6 +4,14 @@
 // functions brakes string into array from delimeter
 // function converts string array to string with camel case
 
-function camelizes(str) {
-  str.split("-");
+function camelize(str) {
+  let words = str.split("-");
+  let camelCase = words.reduce((flat, camel) => {
+    return flat + camel[0].toUpperCase() + camel.slice(1);
+  });
+  return camelCase;
 }
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
