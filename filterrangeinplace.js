72beb3c6bb43357aae3cff2ee60839ee476aac48
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-05 06:55:21 -0500

    solved filterrangeinplace

--- filterrangeinplace.js
+++ filterrangeinplace.js
@@ -4,13 +4,10 @@
 // function changes array value in place
 let arr = [5, 8, 1, 3];
 function filterRangeInPlace(array, a, b) {
-  arr = array.filter((x) => {
-    if (x >= a && x <= b) {
-      return x;
-    } else {
-      //   array.pop();
-    }
-  });
+  filtered = array.filter((x) => x >= a && x <= b);
+
+  array.length = 0;
+  array.push(...filtered);
 }
 filterRangeInPlace(arr, 1, 4);
 console.log(arr);
