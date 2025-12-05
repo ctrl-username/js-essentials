fa49f8d0ad234d17c78b42352746987ded377e1e
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-05 15:41:06 -0500

    iplemented filterrangeinplace function based on the assignment solution

--- filterrangeinplace.js
+++ filterrangeinplace.js
@@ -2,14 +2,38 @@
 //function iterates array values
 // function removes all values from array except ones that are between a and b
 // function changes array value in place
-let arr = [5, 8, 1, 3];
 function filterRangeInPlace(array, a, b) {
-  filtered = array.filter((x) => x >= a && x <= b);
+  const filtered = array.filter((x) => x >= a && x <= b);
 
   // empty array
   array.length = 0;
   // fill array with filtered content
   array.push(...filtered);
 }
-filterRangeInPlace(arr, 1, 4);
+//filterRangeInPlace(arr, 1, 4);
+
+function filterRInPlace(array, a, b) {
+  const filterd = array.filter((x) => x >= a && x <= b);
+  array.splice(0, array.length, ...filterd);
+}
+
+let arr = [5, 8, 1, 3];
+
+// filterRInPlace(arr, 1, 4);
+// console.log(arr);
+
+// assingment solution
+
+function filterRangeInPlaceS(array, a, b) {
+  for (let i = 0; i < array.length; i++) {
+    let val = array[i];
+
+    if (val < a || val > b) {
+      array.splice(i, 1);
+
+      i--;
+    }
+  }
+}
+filterRangeInPlaceS(arr, 1, 4);
 console.log(arr);
