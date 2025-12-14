7f5d7a090316f27e81250586e1f78b590d891b5f
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-14 19:30:06 +0100

    feat: Implemented to do list add function to validate user input function gets user input value function creates new li element in the ul parent function adds userinput value to the li element

--- addlist.js
+++ addlist.js
@@ -17,5 +17,24 @@ let todoListValue;
 const getInputValue = () => {
   return (todoListValue = input.value);
 };
-
+// create add list button
+const addTodoBtn = document.createElement("button");
+addTodoBtn.textContent = "add todo";
+listContainer.append(addTodoBtn);
 // create un ordered list item for adding
+const todoList = document.createElement("ul");
+listContainer.append(todoList);
+
+// create function to add todo
+const addTodo = () => {
+  let todoValue = getInputValue();
+  if (todoValue.length <= 0) {
+    alert("invalid input");
+    return;
+  }
+  let list = document.createElement("li");
+  list.textContent = todoValue;
+  todoList.append(list);
+};
+
+addTodoBtn.addEventListener("click", addTodo);
