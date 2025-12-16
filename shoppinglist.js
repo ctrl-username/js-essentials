1d60b5d15c0b236b6ec5774582b1f2918a46e69c
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-16 20:10:16 +0100

    completed dynamic shopping list program

--- shoppinglist.js
+++ shoppinglist.js
@@ -38,3 +38,34 @@ function addItem(event) {
 }
 
 addButton.addEventListener("click", addItem);
+
+//mdn solution
+// //
+// <script>
+//    const list = document.querySelector('ul');
+//    const input = document.querySelector('input');
+//    const button = document.querySelector('button');
+
+//    button.addEventListener('click', (event) => {
+//      event.preventDefault();
+
+//      const myItem = input.value;
+//      input.value = '';
+
+//      const listItem = document.createElement('li');
+//      const listText = document.createElement('span');
+//      const listBtn = document.createElement('button');
+
+//      listItem.appendChild(listText);
+//      listText.textContent = myItem;
+//      listItem.appendChild(listBtn);
+//      listBtn.textContent = 'Delete';
+//      list.appendChild(listItem);
+
+//      listBtn.addEventListener('click', () => {
+//        list.removeChild(listItem);
+//      });
+
+//      input.focus();
+//    });
+//  </script>
