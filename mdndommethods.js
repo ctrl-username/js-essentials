89a057e4a627f694ccad3ea1b981816223b884ca
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-16 14:19:16 +0100

    feat(js): Create paragraph with quote

--- mdndommethods.js
+++ mdndommethods.js
@@ -2,7 +2,10 @@ const link = document.querySelector("a");
 
 link.textContent = "It's a lonely ride, but we'll get there";
 link.href = "https://raviheima.short.gy";
-
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "Belive you can and you're halfway there";
+sect.appendChild(para);
 //
 //
 //why
